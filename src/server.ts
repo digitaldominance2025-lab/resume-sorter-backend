033fafadf14aedf87b06b2abc44f78646e18494b
Mike <digitaldominance2025@gmail.com>
2026-03-08 09:47:54 -0700

    tune Google Sheets column width to 850px

--- src/server.ts
+++ src/server.ts
@@ -1922,7 +1922,7 @@ await sheets.spreadsheets.batchUpdate({
             startIndex: 0, // A
             endIndex: 5,   // through E
           },
-          properties: { pixelSize: 850 },
+          properties: { pixelSize: 800 },
           fields: "pixelSize",
         },
       },
